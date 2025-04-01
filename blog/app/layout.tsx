230d3c88fe404f90dcf99d9a66419f286292a9b5
Prabin Joshi <joshiprabin17@gmail.com>
2025-04-01 12:09:41 +0545

    update

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const geistSans = Geist({
@@ -24,13 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 
-        {children}
+          {children}
 
-      </body>
-    </html>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- blog/components/dashboard/LeftSideBar.tsx
+++ blog/components/dashboard/LeftSideBar.tsx
@@ -1,3 +1,4 @@
+import { SignOutButton } from '@clerk/nextjs'
 import Link from 'next/link'
 import React from 'react'
 
@@ -7,6 +8,11 @@ export default function LeftSideBar() {
             <Link href="/dashboard">Dashboard</Link>
             <Link href="/dashboard/collections">Collections</Link>
             <Link href="/dashboard/categories">Categories</Link>
+            <Link href="/dashboard/profile">Profile</Link>
+            <div className='mt-auto bg-blue-400 px-8 py-2'>
+                <SignOutButton redirectUrl='/sign-in'></SignOutButton>
+            </div>
+
         </div>
     )
 }
